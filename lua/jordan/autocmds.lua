bc2247f007ff40b92fd71ea16b6a50097cbea72b
Jordan McEntire <jordan@mcentire.dev>
2024-02-27 08:17:57 -0600

    Highlight on yank, skip confirm on oil changes

--- lua/jordan/autocmds.lua
+++ lua/jordan/autocmds.lua
@@ -1,7 +1,15 @@
+-- Enable word wrap and spell checking for gitcommit and markdown files
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "gitcommit", "markdown" },
   callback = function()
     vim.opt_local.wrap = true
     vim.opt_local.spell = true
+  end,
+})
+
+-- Highlight yanked text
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+  callback = function()
+    vim.highlight.on_yank({ higroup = "Visual", timeout = 40 })
   end,
 })
--- lua/jordan/config/oil.lua
+++ lua/jordan/config/oil.lua
@@ -1,6 +1,7 @@
 local M = {
   "stevearc/oil.nvim",
   opts = {
+    skip_confirm_for_simple_edits = true,
     view_options = {
       show_hidden = true,
     },
